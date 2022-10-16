efed1255e043a249a7f1fda45f9aa04f5d4b66b7
poketuk <poketuk@yandex.ru>
2022-10-16 15:57:49 +0300

    Домашнее задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author jackKerouac = new Author( "Jack", "Kerouac");
+        Author iosifBrodsky = new Author("Iosif", "Brodsky");
+        Book onTheRoad = new Book("On the Road", jackKerouac, "1951");
+        System.out.println("Название книги:" + onTheRoad.getTitle() + " Автор:" + onTheRoad.getAuthor().getFullName()
+        + " Дата:" + onTheRoad.getPublicationYear());
     }
 }
