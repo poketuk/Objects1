db7d64e22328bc45ef07a43c098084e3cf01e018
poketuk <poketuk@yandex.ru>
2022-10-16 17:30:45 +0300

    Домашнее задание - Методы объектов

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
     private  final String name;
     private  final String surname;
@@ -12,4 +14,19 @@ public class Author {
     public String getSurname() { return  this.surname; }
 
     public String getFullName() { return  this.name + " " + this.surname; }
+
+    public String toString() { return this.name + " " + this.surname; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(surname, author.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
+    }
 }
--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
 private final String title;
 private final Author author;
@@ -18,5 +20,20 @@ private int publicationYear;
 
     public void setPublicationYear(int publicationYear) {
         this.publicationYear = publicationYear;
+    }
+
+    public String toString() { return this.title + " by " + this.author + " published in " + this.publicationYear; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return publicationYear == book.publicationYear && Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, publicationYear);
     }
 }
